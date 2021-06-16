4c59e1be9901964599d24dd5ad0049dc4c877042
Julian Oes <julian@oes.ch>
2021-06-16 10:45:39 +0200

    mavlink: reduce calls to hrt_absolute_time

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -1834,7 +1834,6 @@ MavlinkReceiver::handle_message_rc_channels_override(mavlink_message_t *msg)
 
 	// fill uORB message
 	input_rc_s rc{};
-	rc.timestamp_last_signal = hrt_absolute_time();
 	rc.rssi = RC_INPUT_RSSI_MAX;
 	rc.rc_total_frame_count = 1;
 	rc.input_source = input_rc_s::RC_INPUT_SOURCE_MAVLINK;
@@ -1876,7 +1875,7 @@ MavlinkReceiver::handle_message_rc_channels_override(mavlink_message_t *msg)
 	}
 
 	// publish uORB message
-	rc.timestamp = hrt_absolute_time();
+	rc.timestamp = rc.timestamp_last_signal = hrt_absolute_time();
 	_rc_pub.publish(rc);
 }
 
@@ -1892,13 +1891,12 @@ MavlinkReceiver::handle_message_manual_control(mavlink_message_t *msg)
 	}
 
 	manual_control_input_s manual{};
-	manual.timestamp_sample = hrt_absolute_time();
 	manual.x = man.x / 1000.0f;
 	manual.y = man.y / 1000.0f;
 	manual.r = man.r / 1000.0f;
 	manual.z = man.z / 1000.0f;
 	manual.data_source = manual_control_input_s::SOURCE_MAVLINK_0 + _mavlink->get_instance_id();
-	manual.timestamp = hrt_absolute_time();
+	manual.timestamp = manual.timestamp_sample = hrt_absolute_time();
 	_manual_control_input_pub.publish(manual);
 }
 
